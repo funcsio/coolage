beaaa17d3808e532978a26bee3522da2aad83ed5
Pranshu Chittora <pranshuchittora17@gmail.com>
2020-11-18 02:37:39 +0530

    feat: Theme change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
+import {
+  createMuiTheme,
+  makeStyles,
+  createStyles,
+  Theme as AugmentedTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { grey } from "@material-ui/core/colors";
+
 import PickerCont from "./PickerContainer";
 import "./App.css";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: grey[900],
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <PickerCont />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <PickerCont />
+      </div>
+    </ThemeProvider>
   );
 }
 
